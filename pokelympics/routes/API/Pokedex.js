5f48727dfd833ec28984997b90f7b15efedfd7da
Zak Butcher <zakbutcher@ZBUTCHER-MBP-DC.local.tld>
2016-08-18 12:55:10 -0500

    Pokedex functionality started

---
+++ pokelympics/routes/API/Pokedex.js
@@ -0,0 +1,33 @@
+var Pokedex = require('pokedex-promise-v2');
+var Q = require('q');
+var P = new Pokedex();
+
+function getPokemon(cb) {
+    pok_prom = [];
+    for (var i = 0; i < 1; i++) {
+        pok_prom.push(P.getPokemonByName(Math.floor((Math.random() * 100) + 1)));
+    }
+
+    Q.allSettled(pok_prom)
+        .then(function (results) {
+            var pok = []
+            results.forEach(function (result) {
+                if (result.state === "fulfilled") {
+                    p = result.value;
+                    stats = {
+                            name: p.name,
+                            speed: p.stats[0].base_stat,
+                            fpic: p.sprites.front_default,
+                            bpick: p.sprites.back_default,
+                    }
+                    pok.push(stats);
+                } else {
+                    var reason = result.reason;
+                    console.log(reason);
+                }
+            });
+            cb(pok);
+        });
+}
+
+module.exports = getPokemon;
--- pokelympics/routes/index.js
+++ pokelympics/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var pokedex = require('./API/Pokedex.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/pok', function(req, res, next) {
+  pokedex(function(pok) {
+    res.json(pok);
+  });
 });
 
 module.exports = router;
